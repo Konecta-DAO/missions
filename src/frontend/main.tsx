da50ac75e5756857d38096f5daa1175694e8335c
capuzr <capuzr@gmail.com>
2024-09-22 21:12:08 -0400

    chore: remove plug and stoic as log in methods.

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -4,7 +4,7 @@ import './index.scss';
 import { canisterId as backId } from '../declarations/backend/index.js';
 import { BrowserRouter } from 'react-router-dom';
 import { IdentityKitProvider } from "@nfid/identitykit/react"
-import { InternetIdentity, NFIDW, Plug, Stoic } from "@nfid/identitykit"
+import { NFIDW } from "@nfid/identitykit"
 import { GlobalProvider } from '../hooks/globalID.tsx';
 import { Routes, Route } from 'react-router-dom';
 import RadialBackground from '../components/RadialBackground/RadialBackground.tsx';
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <GlobalProvider>
       <BrowserRouter>
-        <IdentityKitProvider signers={[NFIDW, Plug, Stoic]} featuredSigner={NFIDW} signerClientOptions={{ derivationOrigin: "https://" + frontId + ".icp0.io", targets: [backId], idleOptions: { idleTimeout: 86400000 }, }} authType={IdentityKitAuthType.DELEGATION}>
+        <IdentityKitProvider signers={[NFIDW]} featuredSigner={NFIDW} signerClientOptions={{ derivationOrigin: "https://" + frontId + ".icp0.io", targets: [backId], idleOptions: { idleTimeout: 86400000 }, }} authType={IdentityKitAuthType.DELEGATION}>
           <UsergeekProvider>
             <RadialBackground>
               <Routes>
